886d03c24ada6f51bffa447536f06fc30afac488
Valeriia <lera_kanavchenko@ukr.net>
2024-08-04 17:02:09 +0300

    Add homework

---
+++ src/js/1-gallery.js
@@ -0,0 +1,29 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { images } from "./gallery-info";
+
+
+const galleryList = document.querySelector('.gallery');
+
+const createGalleryCard = (image) => {
+    return `<li class="gallery-item">
+        <a class="gallery-link" href= "${image.original}">
+            <img
+                class="gallery-image"
+                src = "${image.preview}"
+                alt = "${image.description}"
+            />
+        </a>
+    </li>`;
+}
+
+const createGalleryImages = images.map(imageInfo => createGalleryCard(imageInfo)).join('');
+galleryList.innerHTML = createGalleryImages;
+
+
+let modalGalleryCard = new SimpleLightbox('.gallery .gallery-link', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
+            
+
---
+++ src/js/2-form.js
@@ -0,0 +1,38 @@
+let formData = {
+    email: '',
+    message: '',
+}
+
+const form = document.querySelector('.feedback-form');
+
+function fillFormFromLS() {
+    const infoFromLS = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (infoFromLS === null) {
+        return;
+    }
+
+    formData = infoFromLS;
+
+    for (const key in infoFromLS) {
+        form.elements[key].value = infoFromLS[key];
+    }
+}
+
+fillFormFromLS();
+
+function formFillInfo(event) {
+    if (event.target.name === 'email') {
+        formData.email = event.target.value;
+    } else formData.message = event.target.value;
+
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+function submitForm(event) {
+    event.preventDefault();
+    form.reset();
+    localStorage.removeItem('feedback-form-state');
+}
+
+form.addEventListener('input', formFillInfo);
+form.addEventListener('submit', submitForm);
